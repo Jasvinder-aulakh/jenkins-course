9a55c3ce91797f6974255a5c5198c51a0110d83f
Jass Devops <jass_rhce@live.hk>
2018-05-12 20:36:05 +0530

    Changes

--- job-dsl/nodejs.groovy
+++ job-dsl/nodejs.groovy
@@ -1,8 +1,8 @@
 job('NodeJS example') {
     scm {
-        git('git://github.com/wardviaene/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
+        git('git://github.com/jasvinder-aulakh/nodejs-docker.git') {  node -> // is hudson.plugins.git.GitSCM
             node / gitConfigName('DSL User')
-            node / gitConfigEmail('jenkins-dsl@newtech.academy')
+            node / gitConfigEmail('jasvinder.singh@snapdeal.com')
         }
     }
     triggers {
